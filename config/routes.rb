04db9eabd57a0e41995aef6377bf2568d6a213c9
annabush092 <annabush092@gmail.com>
2018-01-26 20:07:38 -0500

    removed unnecessary routes

--- config/routes.rb
+++ config/routes.rb
@@ -4,9 +4,9 @@ Rails.application.routes.draw do
   namespace :api do
     namespace :v1 do
       resources :users, only: [:index, :show]
-      resources :triggers, only: [:index]
-      resources :trigger_logs, only: [:index]
-      resources :migraines, only: [:index]
+      # resources :triggers, only: [:index]
+      # resources :trigger_logs, only: [:index]
+      # resources :migraines, only: [:index]
     end
   end
 
